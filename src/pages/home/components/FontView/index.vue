678de7870317b8766d274057dafba516754cd9a5
June <1601745371@qq.com>
2024-08-18 13:12:21 +0800

    style: 💄 优化字体列表样式

--- src/pages/home/components/FontView/index.vue
+++ src/pages/home/components/FontView/index.vue
@@ -2,7 +2,7 @@
  * @Author: June
  * @Description: 
  * @Date: 2024-08-16 12:40:31
- * @LastEditTime: 2024-08-16 18:34:49
+ * @LastEditTime: 2024-08-18 13:11:19
  * @LastEditors: June
  * @FilePath: \mobile-fabric-editor\src\pages\home\components\FontView\index.vue
 -->
@@ -15,24 +15,26 @@
     @close="close"
     custom-style="height: 50vh;"
   >
-    <view class="bg-#111 w-full h-full relative overflow-hidden pt-80rpx">
+    <view class="bg-#111 w-full h-full relative overflow-hidden">
       <view
-        class="box-border px-20rpx absolute left-0 right-0 top-0 text-#fff flex justify-between items-center h-80rpx z-1"
+        class="box-border px-20rpx text-#fff flex justify-between items-center h-80rpx"
       >
         <text class="text-28rpx" @click="handleCancel">取消</text>
         <view class="flex-1 text-32rpx text-center font-bold">字体</view>
         <text class="text-28rpx" @click="handleConfirm">确认</text>
       </view>
-      <view
-        class="my-20rpx text-center text-#6a6a6a"
-        :class="{
-          'text-48rpx text-#fff': curIndex === index
-        }"
-        v-for="(font, index) in fontsList"
-        :key="index"
-        @click="handleFont(index)"
-      >
-        {{ font.name }}
+      <view class="w-full overflow-y-auto" style="height: calc(50vh - 80rpx)">
+        <view
+          class="my-20rpx text-center text-#6a6a6a"
+          :class="{
+            'text-48rpx !text-#fff': curIndex === index
+          }"
+          v-for="(font, index) in fontsList"
+          :key="index"
+          @click="handleFont(index)"
+        >
+          {{ font.name }}
+        </view>
       </view>
     </view>
   </wd-popup>
